70668f4445dc09e7839051d444c778f50929a5cf
charlenefialho <fialhocharlene>
2023-10-24 18:13:14 -0300

    feat: style StatusBar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 
@@ -6,10 +7,16 @@ import Groups from "./src/screens/Groups";
 import { Loading } from "@components/Loading";
 
 
+
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent" 
+      translucent
+      />
       {fontsLoaded ? <Groups/> : <Loading/>}
     </ThemeProvider>
     
