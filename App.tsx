899878664cfdb01a99fadfd12bd89eb713f35dbf
charlenefialho <fialhocharlene>
2023-10-24 18:08:12 -0300

    feat: added loading component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 
 import theme from "@theme/index";
-
 import Groups from "./src/screens/Groups";
+import { Loading } from "@components/Loading";
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
   return (
     <ThemeProvider theme={theme}>
-      <Groups/>
+      {fontsLoaded ? <Groups/> : <Loading/>}
     </ThemeProvider>
     
   );
