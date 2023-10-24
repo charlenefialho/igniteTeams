03df9528abbff0a5606267cad44d459de166e9f0
charlenefialho <fialhocharlene>
2023-10-24 19:34:19 -0300

    feat: added button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
+import { Button } from '@components/Button';
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from "./styles";
-
-
 
 export default function Groups(){
   const [groups, setGroups] = useState<string[]>([]);
@@ -31,6 +30,10 @@ export default function Groups(){
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
     </Container>
     
