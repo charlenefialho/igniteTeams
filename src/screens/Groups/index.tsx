71e57c2c3daf36efea0f61ee7ad84e5b699f5eec
charlenefialho <fialhocharlene>
2023-10-24 19:20:04 -0300

    feat: added message when list is void

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
-import { GroupCard } from "@components/GroupCard"
-import { Header } from "@components/Header"
-import { Highlight } from "@components/Highlight"
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from '@components/ListEmpty';
 
-import { Container } from "./styles"
+import { Container } from "./styles";
+
 
 
 export default function Groups(){
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
   return(
     <Container>
       <Header />
@@ -24,6 +26,10 @@ export default function Groups(){
           <GroupCard 
             title={item} 
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
     </Container>
